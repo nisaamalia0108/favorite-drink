3b0ac77ad2dd31743e30645614b42c0b2bd82d08
nisaamalia0108 <nisaamalia0108@gmail.com>
2026-06-10 22:51:23 +0700

    first commit

---
+++ drink.js
@@ -0,0 +1,12 @@
+let drink1 = "Cappuccino";
+let drink2 = "Matcha Latte";
+let drink3 = "Thai Tea";
+let drink4 = "Pistachio Latte";
+let drink5 = "Chocolate Milkshake";
+
+console.log("Daftar Minuman Favorit:");
+console.log(drink1);
+console.log(drink2);
+console.log(drink3);
+console.log(drink4);
+console.log(drink5);
